83d243d9f4fedfc46356d083384786c57d6b882d
Chrisgraham0104 <chrisgraham0104@gmail.com>
2018-04-04 13:46:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ function () {
 });  
 // chat connector for communicating with the Bot Framework Service 
 var connector = new builder.ChatConnector({     
-    appId: process.env.MICROSOFT_APP_ID,     
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID || 'dd0a1b2d-d32f-46dc-a668-c9e848f84b12',     
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || 'hFFQ488#+#okechbJRPY51~' 
 });
 // Listen for messages from users  
 server.post('/api/messages', connector.listen());  
