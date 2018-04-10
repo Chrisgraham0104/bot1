48cafddc0218a3c7a59faeaf959f11b7a98dd5fd
Chrisgraham0104 <chrisgraham0104@gmail.com>
2018-04-10 16:48:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -271,7 +271,8 @@ bot.dialog('askExecution', [
                         /* end Date format*/
                         //shortCountry =  body.recordset[0].countrypseudo;
 
-                        fname = branch + '_' + customerid + fdate + '.xlsx';
+                        //fname = branch + '_' + customerid + fdate + '.xlsx';
+			    fname = branch + '_' + customerid + fdate + '.txt';
                         ///xfilepath = '.\\' + configDataArr.Folder + '\\' + fname;
                         //xfilepath = '.\\' + 'xls_data' + '\\' + fname;
 			    xfilepath = './' + 'xls_data' + '/' + fname;
